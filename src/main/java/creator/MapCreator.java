fd629be8ffe5ab6a150c4f73c5436573518df025
Nicola <Nicola@10.169.150.226>
2015-05-19 12:22:32 +0200

    simplify mapCreator

--- src/main/java/creator/MapCreator.java
+++ src/main/java/creator/MapCreator.java
@@ -10,26 +10,9 @@ public class MapCreator {
 		
 		Map map = null;
 		LoadExagonalMap loadExagonalmap = new LoadExagonalMap();
-		
-		
-		switch(mapName) {
-			case "Fermi": 
-			try {
-				map = loadExagonalmap.loadMap("FermiMap.txt");
-			} catch (NumberFormatException | IOException e1) {}
-			break;
-			case "Galilei": try {
-				map = loadExagonalmap.loadMap("GalileiMap.txt");
-			} catch (NumberFormatException | IOException e1) {}
-			break;
-			case "Galvani": try {
-				map = loadExagonalmap.loadMap("GalvaniMap.txt");
-			} catch (NumberFormatException | IOException e1) {}
-			break;
-			default: try {
-				map = loadExagonalmap.loadMap("GalileiMap.txt");
-			} catch (NumberFormatException | IOException e1) {}
-		}
+		try {
+			map=loadExagonalmap.loadMap(mapName);
+		} catch (NumberFormatException | IOException e) {}
 		return map;
 	}
 }
