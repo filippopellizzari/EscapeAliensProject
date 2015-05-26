7af87535c003aa79cfead0946fcb82d8b833706d
Filippo Pellizzari <filippopellizzari@195.168.1.203>
2015-05-27 01:21:33 +0200

    fix javadoc testmove

--- src/test/java/testController/TestMove.java
+++ src/test/java/testController/TestMove.java
@@ -12,161 +12,157 @@ import model.*;
 import controller.*;
 
 /**
- * Test sulla validazione di una mossa dei giocatori (spostamento sulla mappa "Galilei")
+ * Test of move check and action move of players(eg. Galilei Map)
+ * 
  * @author Filippo
  *
  */
 public class TestMove {
 
-	
 	static Game model;
 	static Player human;
 	static Player alien;
-	static Player alienFed; //alieno che è riuscito ad eliminare almeno un umano
-	
-	
-	@BeforeClass public static void onlyOnce() throws NumberFormatException, IOException {
+	static Player alienFed; // alieno che è riuscito ad eliminare almeno un
+							// umano
+
+	@BeforeClass
+	public static void onlyOnce() throws NumberFormatException, IOException {
 		model = GameCreator.getinstance().createGame("Galilei", 8, "Exagonal");
-	
-		model.getPlayers(0).setPlayerType(PlayerType.HUMAN); 
+
+		model.getPlayers(0).setPlayerType(PlayerType.HUMAN);
 		model.getPlayers(0).setSpeed(1);
 		human = model.getPlayers(0);
-		
+
 		model.getPlayers(1).setPlayerType(PlayerType.ALIEN);
 		model.getPlayers(1).setSpeed(2);
 		alien = model.getPlayers(1);
-	
+
 		model.getPlayers(2).setPlayerType(PlayerType.ALIEN);
 		model.getPlayers(2).setSpeed(3);
 		alienFed = model.getPlayers(2);
 	}
-	
+
 	/**
-	 * il test verifica che un umano non può spostarsi di due settori (non sta usando l'adrenalina)
+	 * The test verifies that a human can not move across two sectors ( not
+	 * using adrenaline )
 	 */
 	@Test
 	public void testHumanMove() {
 		GameStatus status = new GameStatus(model, human);
-		Coordinate curr = new Coordinate(12,5);
+		Coordinate curr = new Coordinate(12, 5);
 		Sector s = model.getMap().getSector(curr);
 		human.setSector(s);
-		
-		Coordinate dest = new Coordinate (12,3);
+
+		Coordinate dest = new Coordinate(12, 3);
 		assertFalse(new Move(status).moveCheck(dest));
 	}
-	
+
 	/**
-	 * il test verifica che un alieno può spostarsi di due settori (non ha ancora eliminato nessuno)
+	 * The test verifies that an alien can move across two sectors ( has not
+	 * eliminated anyone)
 	 */
 	@Test
 	public void testAlienMove() {
 		GameStatus status = new GameStatus(model, alien);
-		Coordinate curr = new Coordinate(12,5);
+		Coordinate curr = new Coordinate(12, 5);
 		Sector s = model.getMap().getSector(curr);
 		alien.setSector(s);
-		
-		Coordinate dest = new Coordinate (12,3);
+
+		Coordinate dest = new Coordinate(12, 3);
 		assertTrue(new Move(status).moveCheck(dest));
 	}
-	
+
 	/**
-	 * il test verifica che un alieno non può spostarsi su un settore scialuppa
-	 */
+	 * The test verifies that an alien can not move to an escape hatch sector
+	 **/
+
 	@Test
 	public void testAlienMoveToHatch() {
 		GameStatus status = new GameStatus(model, alien);
-		Coordinate curr = new Coordinate(4,3);
+		Coordinate curr = new Coordinate(4, 3);
 		Sector s = model.getMap().getSector(curr);
 		alien.setSector(s);
-		
-		Coordinate dest = new Coordinate (2,2);
+
+		Coordinate dest = new Coordinate(2, 2);
 		assertFalse(new Move(status).moveCheck(dest));
-		
+
 	}
-	
+
 	/**
-	 * il test verifica che un alieno "nutrito" può spostarsi di 3 settori
+	 * The test verifies that an alien "fed" can move through 3 sectors
+	 * 
 	 */
 	@Test
 	public void testAlienFedMove() {
 		GameStatus status = new GameStatus(model, alienFed);
-		Coordinate curr = new Coordinate(12,5);
+		Coordinate curr = new Coordinate(12, 5);
 		Sector s = model.getMap().getSector(curr);
 		alienFed.setSector(s);
-		
-		Coordinate dest = new Coordinate (13,6);
+
+		Coordinate dest = new Coordinate(13, 6);
 		assertTrue(new Move(status).moveCheck(dest));
 	}
-	
+
 	/**
-	 * il test verifica che un generico giocatore (ad es. alieno) non possa raggiungere 
-	 * un settore non presente sulla mappa (anche se rispetta la distanza)
+	 * The test verifies that a generic player ( eg . Alien ) can not reach a
+	 * null sector on the map ( even if he respects the distance )
 	 */
-	@Test 
+	@Test
 	public void testMoveToNull() {
 		GameStatus status = new GameStatus(model, alien);
-		Coordinate curr = new Coordinate(6,5);
+		Coordinate curr = new Coordinate(6, 5);
 		Sector s = model.getMap().getSector(curr);
 		alien.setSector(s);
-		
-		Coordinate dest = new Coordinate (4,4);
-		assertFalse(new Move(status).moveCheck(dest));	
+
+		Coordinate dest = new Coordinate(4, 4);
+		assertFalse(new Move(status).moveCheck(dest));
 	}
-	
+
 	/**
-	 *il test verifica che un generico giocatore (ad es. alieno) non possa raggiungere 
-	 *un settore della mappa attraversando settori "nulli" (anche se rispetta la distanza)
+	 * the test verifies that a generic player (eg . alien ) can not reach a
+	 * sector of the map across null sectors " null " (even if it respects the
+	 * distance)
 	 */
-	@Test 
+	@Test
 	public void testMoveThroughNull() {
 		GameStatus status = new GameStatus(model, alien);
-		Coordinate curr = new Coordinate(5,5);
+		Coordinate curr = new Coordinate(5, 5);
 		Sector s = model.getMap().getSector(curr);
 		alien.setSector(s);
-		
-		Coordinate dest = new Coordinate (3,4);
-		assertFalse(new Move(status).moveCheck(dest));	
+
+		Coordinate dest = new Coordinate(3, 4);
+		assertFalse(new Move(status).moveCheck(dest));
 	}
-	
+
 	/**
-	 * il test verifica che un generico giocatore(ad es. alieno) non possa 
-	 * raggiungere un settore "chiuso"(es. settore umano di partenza),
-	 * anche se rispetta la distanza
+	 * the test verifies that a generic player (eg . alien) can not reach a
+	 * sector " closed " (eg. human sector) , even if it respects the distance
 	 */
 	@Test
 	public void testMoveToClosedSector() {
 		GameStatus status = new GameStatus(model, alien);
-		Coordinate curr = new Coordinate(14,9);
+		Coordinate curr = new Coordinate(14, 9);
 		Sector s = model.getMap().getSector(curr);
 		alien.setSector(s);
-		
+
 		Coordinate dest = model.getMap().getHumanCoord();
 		assertFalse(new Move(status).moveCheck(dest));
 	}
-	
 
 	/**
-	 *il test verifica che un generico giocatore (ad es. alieno) non possa raggiungere
-	 *un settore della mappa attraversando un settore "chiuso" (es. settore umano di partenza),
-	 *anche se rispetta la distanza 
+	 * The test verifies that a generic player (eg. alien) can not reach a
+	 * sector of the map moving across a sector " closed " (eg. human sector) ,
+	 * even if it respects the distance
 	 */
 	@Test
 	public void testMoveThroughClosedSector() {
 		GameStatus status = new GameStatus(model, alien);
-		Coordinate curr = new Coordinate(13,8);
+		Coordinate curr = new Coordinate(13, 8);
 		Sector s = model.getMap().getSector(curr);
 		alien.setSector(s);
-		
-		Coordinate dest = new Coordinate (11,9);
+
+		Coordinate dest = new Coordinate(11, 9);
 		assertFalse(new Move(status).moveCheck(dest));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
