05fe3a1748643888ae0247bcdc0247c80cad6b6e
Filippo Pellizzari <filippopellizzari@195.168.1.203>
2015-06-16 23:32:35 +0200

    fix gameStatus

--- src/main/java/controller/GameStatus.java
+++ src/main/java/controller/GameStatus.java
@@ -5,8 +5,9 @@ import java.io.Serializable;
 import model.*;
 
 /**
- * this class contains the status of a player during his turn; booleans attributes indicate
- * if a player has already do a type of action or if a player must do something
+ * this class contains the status of a player during his turn; booleans
+ * attributes indicate if a player has already do a type of action or if a
+ * player must do something
  * 
  * @author Filippo
  *
@@ -20,10 +21,10 @@ public class GameStatus implements Serializable {
 	private boolean moved;
 	private boolean attacked;
 	private boolean sedated;
-	private boolean mustDraw; //must draw dangerous sector card
-	private boolean mustDiscardItem;//must discard fourth itemcard
-	private boolean mustNoise; //must select random coordinate for "noise in any sector"
-	
+	private boolean mustDraw; // must draw dangerous sector card
+	private boolean mustDiscardItem;// must discard fourth itemcard
+	private boolean mustNoise; // must select random coordinate for
+								// "noise in any sector"
 
 	public GameStatus(Game game, Player player) {
 		this.player = player;
@@ -35,74 +36,60 @@ public class GameStatus implements Serializable {
 		mustNoise = false;
 	}
 
-
 	public Player getPlayer() {
 		return player;
 	}
-
-
 
 	public Game getGame() {
 		return game;
 	}
 
-
 	public boolean isMoved() {
 		return moved;
 	}
-
 
 	public void setMoved(boolean moved) {
 		this.moved = moved;
 	}
 
-
 	public boolean isAttacked() {
 		return attacked;
 	}
-
 
 	public void setAttacked(boolean attacked) {
 		this.attacked = attacked;
 	}
 
-
-	public boolean isSedated(){
+	public boolean isSedated() {
 		return sedated;
 	}
-	
-	public void setSedated(boolean sedated){
+
+	public void setSedated(boolean sedated) {
 		this.sedated = sedated;
 	}
-	
+
 	public boolean isMustDraw() {
 		return mustDraw;
 	}
-
 
 	public void setMustDraw(boolean mustDraw) {
 		this.mustDraw = mustDraw;
 	}
 
-
 	public boolean isMustDiscardItem() {
 		return mustDiscardItem;
 	}
-
 
 	public void setMustDiscardItem(boolean mustDiscardItem) {
 		this.mustDiscardItem = mustDiscardItem;
 	}
 
-
 	public boolean isMustNoise() {
 		return mustNoise;
 	}
-
 
 	public void setMustNoise(boolean mustNoise) {
 		this.mustNoise = mustNoise;
 	}
 
-	
 }
