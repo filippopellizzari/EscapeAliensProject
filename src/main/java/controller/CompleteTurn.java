374c4b167dab8988dea48396a6d4cb9e873f15ba
Filippo Pellizzari <filippopellizzari@195.168.1.203>
2015-06-16 23:57:10 +0200

    fix completeTurn format

--- src/main/java/controller/CompleteTurn.java
+++ src/main/java/controller/CompleteTurn.java
@@ -42,15 +42,16 @@ public class CompleteTurn {
 	 * 
 	 * @param dtoGameList
 	 * @return dtoGameList, list of all the actions done
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws ClassNotFoundException
 	 */
 
-	public List<DTOGame> completeTurn() throws ClassNotFoundException, InstantiationException, IllegalAccessException{
+	public List<DTOGame> completeTurn() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
 		random = new Random();
-		//ciclo finchè tutte le 4 azioni obbligatorie
-		//non sono state completate a random
+		// ciclo finchè tutte le 4 azioni obbligatorie
+		// non sono state completate a random
 		do {
 			condizione = 0;
 			checkMove();
@@ -58,14 +59,16 @@ public class CompleteTurn {
 			checkMustDraw();
 			checkMustNoise();
 		} while (condizione < 4);
-		
+
 		return dtoGameList;
 	}
 
-	private void checkMove() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	private void checkMove() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
 		if (!status.isMoved()) {
-			Coordinate randomCoord = status.getPlayer().getSector().getAdjacent().get(random.nextInt(6));
-			if(!status.getGame().getMap().isNull(randomCoord)){
+			Coordinate randomCoord = status.getPlayer().getSector()
+					.getAdjacent().get(random.nextInt(6));
+			if (!status.getGame().getMap().isNull(randomCoord)) {
 				dtoTurn = new DTOTurn(randomCoord, null, ActionType.MOVE);
 				dtoGame = new DTOGame();
 				dtoGame = currentTurn.action(dtoTurn);
@@ -76,9 +79,11 @@ public class CompleteTurn {
 		}
 	}
 
-	private void checkMustDiscardItem() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	private void checkMustDiscardItem() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
 		if (status.isMustDiscardItem()) {
-			ItemCardType randomType = status.getPlayer().getItem().get(random.nextInt(4)).getType();
+			ItemCardType randomType = status.getPlayer().getItem()
+					.get(random.nextInt(4)).getType();
 			dtoTurn = new DTOTurn(null, randomType, ActionType.DISCARDITEM);
 			dtoGame = new DTOGame();
 			dtoGame = currentTurn.action(dtoTurn);
@@ -89,9 +94,10 @@ public class CompleteTurn {
 
 	}
 
-	private void checkMustDraw() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	private void checkMustDraw() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
 		if (status.isMustDraw()) {
-			dtoTurn = new DTOTurn(null, null,ActionType.DRAWSECTORCARD);
+			dtoTurn = new DTOTurn(null, null, ActionType.DRAWSECTORCARD);
 			dtoGame = new DTOGame();
 			dtoGame = currentTurn.action(dtoTurn);
 			dtoGameList.add(dtoGame);
@@ -100,11 +106,14 @@ public class CompleteTurn {
 		}
 	}
 
-	private void checkMustNoise() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	private void checkMustNoise() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
 		if (status.isMustNoise()) {
-			Coordinate randomCoord = new Coordinate(random.nextInt(22) + 1,random.nextInt(13) + 1); 									
-			if(!status.getGame().getMap().isNull(randomCoord)){
-				dtoTurn = new DTOTurn(randomCoord, null, ActionType.SELECTSECTORNOISE);
+			Coordinate randomCoord = new Coordinate(random.nextInt(22) + 1,
+					random.nextInt(13) + 1);
+			if (!status.getGame().getMap().isNull(randomCoord)) {
+				dtoTurn = new DTOTurn(randomCoord, null,
+						ActionType.SELECTSECTORNOISE);
 				dtoGame = new DTOGame();
 				dtoGame = currentTurn.action(dtoTurn);
 				dtoGameList.add(dtoGame);
@@ -113,16 +122,9 @@ public class CompleteTurn {
 			condizione++;
 		}
 	}
-	
-	public GameStatus getStatus(){
+
+	public GameStatus getStatus() {
 		return status;
 	}
 
-	public List<DTOGame> getDtoGameList() {
-		return dtoGameList;
-	}
-	
-	
-	
-	
 }
