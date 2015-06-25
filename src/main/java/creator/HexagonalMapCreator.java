b0d2ca92d2ac7c261219f13bea4b540b5c22b0f0
Filippo Pellizzari <filippopellizzari@195.168.1.203>
2015-06-25 18:49:03 +0200

    fix javadoc

--- src/main/java/creator/HexagonalMapCreator.java
+++ src/main/java/creator/HexagonalMapCreator.java
@@ -10,84 +10,102 @@ import connection.MapName;
 
 /**
  * This class load an exagonal map from the corresponding file
+ * 
  * @author Nicola
  *
  */
 
-public class HexagonalMapCreator extends MapCreator{
-	
+public class HexagonalMapCreator extends MapCreator {
+
+	/**
+	 * Initializes list of hatchSectors and Array of Sectors
+	 */
 	public HexagonalMapCreator() {
 		this.hatchSectors = new ArrayList<Coordinate>();
 		this.sectors = new Sector[322];
 	}
-	
+
 	/**
 	 * 
 	 * @param fileName
 	 * @return a new exagonal map
-	 * @throws NumberFormatException  thrown to indicate that the application has attempted 
-	 * to convert a string to one of the numeric types, 
-	 * but that the string does not have the appropriate format
-	 * @throws IOException  signals that an I/O exception of some sort has occurred
+	 * @throws NumberFormatException
+	 *             thrown to indicate that the application has attempted to
+	 *             convert a string to one of the numeric types, but that the
+	 *             string does not have the appropriate format
+	 * @throws IOException
+	 *             signals that an I/O exception of some sort has occurred
 	 */
-	
-	@Override
-	public Map loadMap(MapName mapName) throws NumberFormatException, IOException {
 
-		File file = new File("rsc"+File.separatorChar+mapName+"Map.txt");
-		
+	@Override
+	public Map loadMap(MapName mapName) throws NumberFormatException,
+			IOException {
+
+		File file = new File("rsc" + File.separatorChar + mapName + "Map.txt");
+
 		FileReader fileRead = new FileReader(file);
-		BufferedReader br = new BufferedReader(fileRead); 
-		
+		BufferedReader br = new BufferedReader(fileRead);
+
 		SectorType sectorType;
-		
+
 		String s;
-		while((s = br.readLine()) != null) {
-				
-			//assegnazione del tipo di settore
-			switch(s) {
-				case "Alien": sectorType = SectorType.ALIEN;
+		while ((s = br.readLine()) != null) {
+
+			// assegnazione del tipo di settore
+			switch (s) {
+			case "Alien":
+				sectorType = SectorType.ALIEN;
 				break;
-				case "Human": sectorType = SectorType.HUMAN;
+			case "Human":
+				sectorType = SectorType.HUMAN;
 				break;
-				case "Hatch": sectorType = SectorType.HATCH;
+			case "Hatch":
+				sectorType = SectorType.HATCH;
 				break;
-				case "Dangerous": sectorType = SectorType.DANGEROUS;
+			case "Dangerous":
+				sectorType = SectorType.DANGEROUS;
 				break;
-				default: sectorType = SectorType.SECURE;
+			default:
+				sectorType = SectorType.SECURE;
 				break;
 			}
-			
-			boolean closed = Boolean.parseBoolean(br.readLine()); 
-			
+
+			boolean closed = Boolean.parseBoolean(br.readLine());
+
 			int x = Integer.parseInt(br.readLine());
 			int y = Integer.parseInt(br.readLine());
-			
-			switch(sectorType) {
-				case ALIEN: this.alienSector = new Coordinate(x,y);
+
+			switch (sectorType) {
+			case ALIEN:
+				this.alienSector = new Coordinate(x, y);
 				break;
-				case HUMAN: this.humanSector = new Coordinate(x,y);
+			case HUMAN:
+				this.humanSector = new Coordinate(x, y);
 				break;
-				case HATCH: hatchSectors.add(new Coordinate(x,y));
+			case HATCH:
+				hatchSectors.add(new Coordinate(x, y));
 				break;
-				default: break;
+			default:
+				break;
 			}
-			
-			//6 settori adiacenti a un settore
+
+			// 6 settori adiacenti a un settore
 			List<Coordinate> adjacent = new ArrayList<Coordinate>();
-			for(int i = 0; i < 6; i++){
-				adjacent.add(new Coordinate(Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine())));
+			for (int i = 0; i < 6; i++) {
+				adjacent.add(new Coordinate(Integer.parseInt(br.readLine()),
+						Integer.parseInt(br.readLine())));
 			}
-			//creazione concreta del settore
-			if(sectorType != SectorType.HATCH) {
-				sectors[(y-1)*23 + (x-1)]= new Sector(sectorType, closed, x, y, adjacent);
-			}
-			else {
-				sectors[(y-1)*23 + (x-1)] = new HatchSector(sectorType, closed, x, y, adjacent);		
+			// creazione concreta del settore
+			if (sectorType != SectorType.HATCH) {
+				sectors[(y - 1) * 23 + (x - 1)] = new Sector(sectorType,
+						closed, x, y, adjacent);
+			} else {
+				sectors[(y - 1) * 23 + (x - 1)] = new HatchSector(sectorType,
+						closed, x, y, adjacent);
 			}
 		}
 		br.close();
 		return new HexagonalMap(sectors, humanSector, alienSector, hatchSectors);
-		
+
 	}
 }
